7bc95fbb86efce952c3232a2da3a3b2e1ccfd658
schnell7 <schnell7@students.uni-marburg.de>
2018-02-05 21:42:56 +0100

    _

--- GIS/abgabe/singletreedetect.R
+++ GIS/abgabe/singletreedetect.R
@@ -1,24 +1,48 @@
 #' @title Single tree detection using LAS files
 #' 
-#' @describtion this function allows you to detect singele trees using LiDar technology. You need some kind of normalized canopy height model "CHM" as well as .las files first
+#' @describtion this function allows you to detect singele trees using LiDar technology. You need some kind of normalized canopy height model "CHM" as well as .las files first \cr 
+#' the output is 1 ESRI shapefile per "treeheight" for method 'watershed' and all combinations bewteen "treeheight" and "crowndm" for method 'dalponte2016', as well as one .csv file summarizing your results per run \cr 
+#' ! save your .csv file after each run !
 #' 
 #' @author Laura Giese, Johannes Schnell & Alena Schmid
 #' 
-#' @note both 'dalponte2016' and 'watershed' are used as methods to detect single trees
-#' 
+#' @note both 'dalponte2016' and 'watershed' are used as methods to detect single trees \cr required packages are: lidR, rgdal & raster \cr
+#'you also need 'EBImage' which can be installed using the instruction below or follow instructions \href<-{"https://www.bioconductor.org/"}{here}  \cr
+#'\code{
+#'"## try http:// if https:// URLs are not supported
+#'source("https://bioconductor.org/biocLite.R")
+#'biocLite("EBImage")
+#'install.packages("EBImage")"
+#'} \cr
+#'! make sure to save the produced .csv file after each run !
 #' @param chm is the normalized canopy heigt model 
 #' @param las is the underlaying .las file
-#' @param output is the filename to save results (ESRI shapefiles)
-#' @param treeheight is the maximum height an object will be considered a 'tree' [m] (try different values)
-#' @param crowndm is the crowndiameter [m] (try different values), preset is 'NULL'
-#' @param overwrite TRUE or FALSE. Do you want to override existing outputs?, preset is 'TRUE'
+#' @param output is the path where you want to save the results
+#' @param treeheight is the minimum height an object will be considered as 'treecrown' [m] (try different values see example)
+#' @param crowndm is the crowndiameter [m] (try different values see example); preset is 'NULL'
+#' @param overwrite TRUE or FALSE. Do you want to override existing outputs?; preset is 'TRUE'
 #' 
 #' @export runsingletreedetectlidr
+#' @examples saves and overwrites (if existing) the results in './temp/' with different iterations of treeheight (bewteen 5 and 10m, by 1m stpes) and different crowndiameters (5,7,10,15,20,25) \cr
+#' will produce 36 .shp files 'dalponte2016' and 6 .shp files 'watershed' as well as one .csv file 
+#' \dontrun{
+#'runSingleTreeDetectionLidR(chm = your_chm, las = you_las_files, output = temp,
+#'                                         treeheight = c(5:10), crowndm = c(5,7,10,15,20,25), overwrite = TRUE)
+#' }
+#' 
+#' 
+#'##--------------------------------------------------------------### 
+#'## 3. functions for single tree detection-----------------------###
+#'##steps 0-2 are preperation check out the github folder "abgabe"###
+#'##--------------------------------------------------------------###
 
-###--------------------------------------------------------------### 
-### 3. functions for single tree detection-----------------------###
-###steps 0-2 are preperation check out the github folder "abgabe"###
-###--------------------------------------------------------------###
+#required libraries
+require(lidR)
+require(raster)
+require(rgdal)
+
+require(EBImage)
+
 
 #define function1 SensitivityAnalysis which is part of the final function
 SensitivityAnalysis = function(chm, las,
@@ -115,6 +139,6 @@ runSingleTreeDetectionLidR = function(chm, las, output,
   #save files and output table
   sa_files<- rbind(SensAnalysisFiles_d, SensAnalysisFiles_w)
   colnames(sa_files)<- c("filename", "method", "mintreeheight", "crowndiameter", "time", "treenumber")
-  write.table(sa_files, file = paste0(temp, "SensAnalysisFiles_out.txt"), sep =",")
+  write.table(sa_files, file = paste0(temp, Sys.Date(), "SensAnalysisFiles_out.csv"), sep =",", overwrite=overwrite)
 }
 
